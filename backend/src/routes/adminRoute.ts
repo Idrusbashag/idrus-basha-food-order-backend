4b9c72d54a367bbc83bd96545488b5006f9bf7bf
Idrusbashag <117631583+Idrusbashag@users.noreply.github.com>
2023-01-13 18:00:57 +0530

    render

--- backend/src/routes/adminRoute.ts
+++ backend/src/routes/adminRoute.ts
@@ -94,7 +94,7 @@ function getTime() {
 var storage = multer.diskStorage({
 
     destination: (req, file, callBack) => {
-        callBack(null, 'https://idrus-basha-food-order-frontend.onrender.com/dist/assets/pic')
+        callBack(null, '../../assets/pizza')
     },
     filename: (req, file, callBack) => {
         callBack(null, `${getTime()}-${file.originalname}`)
