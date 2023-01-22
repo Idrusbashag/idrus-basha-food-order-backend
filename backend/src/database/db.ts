d2079ca4cebfde3576473c03466a39438d0ab416
Idrusbashag <117631583+Idrusbashag@users.noreply.github.com>
2023-01-22 23:03:23 +0530

    render

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 require('dotenv').config()
 mongoose.Promise=global.Promise
-mongoose.connect("mongodb+srv://idrusbasha:basha@cluster0.xp7pfy6.mongodb.net/Foodapp?retryWrites=true&w=majority")
+mongoose.connect("mongodb://idrusbasha:basha@ac-dkqtsux-shard-00-00.xp7pfy6.mongodb.net:27017,ac-dkqtsux-shard-00-01.xp7pfy6.mongodb.net:27017,ac-dkqtsux-shard-00-02.xp7pfy6.mongodb.net:27017/Foodapp?ssl=true&replicaSet=atlas-954rpz-shard-0&authSource=admin&retryWrites=true&w=majority")
 mongoose.set('strictQuery', true);
 const con = mongoose.connection
 con.on('open', () => {
